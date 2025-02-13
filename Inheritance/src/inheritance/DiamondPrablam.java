79fda425f6a0dd13ddfbe97ddfb0869c42c25756
ayushsahu007 <ayushsahu007ay@gmail.com>
2025-02-13 21:04:42 +0530

    Inheritance

--- Inheritance/src/inheritance/DiamondPrablam.java
+++ Inheritance/src/inheritance/DiamondPrablam.java
@@ -1,4 +1,40 @@
 package inheritance;
 
+// Base interface (acts as ClassA)
+interface A {
+    default void show() {
+        System.out.println("This is Super Interface ");
+    }
+}
+
+// Interface extending A (acts as ClassB)
+interface B extends A {
+    default void show() {
+        System.out.println("This is 1st Sub Interface ");
+    }
+}
+
+// Another interface extending A (acts as ClassC)
+interface C extends A {
+    default void show() {
+        System.out.println("This is 2nd Sub Interface");
+    }
+}
+
+// ClassD implements both B and C, resolving the conflict
+class D implements B, C {
+    @Override
+    public void show() {
+        System.out.println("This is Sub Class");
+        B.super.show(); // Explicitly calling B's show()
+        C.super.show(); // Explicitly calling C's show()
+    }
+}
+
+// Main Class
 public class DiamondPrablam {
+    public static void main(String[] args) {
+        D obj = new D();
+        obj.show(); // Calls the resolved method
+    }
 }
--- Inheritance/src/inheritance/MultipleInheitace.java
+++ Inheritance/src/inheritance/MultipleInheitace.java
@@ -1,4 +1,29 @@
 package inheritance;
 
+interface Vehicle{
+    default void start(){
+        System.out.println("Vehicle Start");
+    }
+}
+
+interface Car1 extends Vehicle{
+    default void start(){
+        System.out.println("Car Started ");
+    }
+}
+interface Bus extends Vehicle{
+    default void start(){
+        System.out.println("Bus Started");
+    }
+}
+class Engine implements Car1 ,Bus{
+
+    @Override
+    public void start() {
+        Car1.super.start();
+    }
+}
+
+
 public class MultipleInheitace {
 }
